a1fa9ab4a1d7830054f03ade700e79e899f1b804
tnals98 <sumin090598@gmail.com>
2023-07-06 17:39:13 +0900

    [ADD] 23/07/06 자바 api 패키지와 모듈, 기본 패키지
    1. 패키지와 모듈 개념
    2. 기본패키지 클래스(메소드) 실습
     - Wrapper (Boxing & UnBoxing)
     - String, StringTokenizer, StringBuffer
     - Date
     - Calendar

---
+++ src/com09/wrapperpkg/Exam_Wrapper.java
@@ -0,0 +1,31 @@
+package com09.wrapperpkg;
+
+public class Exam_Wrapper {
+	public static void main(String[]args) {
+		int num = 0;
+		Integer su= Integer.valueOf(0);
+//		The constructor Integer(int) has been deprecated since version 9 and marked for removal
+//		su = new Integer(0);
+//		su = null;
+		// 박싱과 언박싱
+		su = Integer.valueOf(1026);
+		su = 1026; //토오-박싱
+		
+		int suNum = su.intValue();
+		suNum = su; // 오토-언박싱
+		
+		String openDay = "20230515";
+		int openDate = Integer.parseInt(openDay);
+		
+		System.out.println(Character.toLowerCase('A'));
+		System.out.println(Character.toUpperCase('a'));
+		
+		char c1 = '4', c2 = 'F';
+		if(Character.isDigit(c1)) {
+			System.out.println(c1 + "은 숫자");
+		}
+		if(Character.isAlphabetic(c2)) {
+			System.out.println(c2 + "는 영문자");
+		}
+	}
+}
