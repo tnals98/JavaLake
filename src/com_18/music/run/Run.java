8651e40e5d8ae472a9ebcd822f92947ab38c574e
tnals98 <sumin090598@gmail.com>
2023-07-19 16:42:31 +0900

    [ADD] 23/07/19 음악관리 프로그램 실습 완료
    1. run - view - controller - dao 구조를 이용하여 음악관리 프로그램 실습 완료
    2. MVC패턴 적용 완료

---
+++ src/com_18/music/run/Run.java
@@ -0,0 +1,10 @@
+package com_18.music.run;
+
+import com_18.music.view.MusicView;
+
+public class Run {
+	public static void main(String[] args) {
+		MusicView view = new MusicView();
+		view.startProgram();
+	}
+}
